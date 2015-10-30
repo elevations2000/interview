c604a2743f9a7702e2a86f026920b6923b9dbd92
85a7af8ca55e0c752083358013f1edcd <randallventrone@live.com>
2015-10-29 23:51:24 -0400

    SOS Interview

---
+++ 85a7af8ca55e0c752083358013f1edcd/hourglass/hourglass/Form1.Designer.vb
@@ -0,0 +1,149 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmHourGlass
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCreate = New System.Windows.Forms.Button()
+        Me.txtHeight = New System.Windows.Forms.TextBox()
+        Me.txtCapacity = New System.Windows.Forms.TextBox()
+        Me.txtHourGlass = New System.Windows.Forms.TextBox()
+        Me.lblHeight = New System.Windows.Forms.Label()
+        Me.lblCapacity = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblError1 = New System.Windows.Forms.Label()
+        Me.lblError2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnCreate
+        '
+        Me.btnCreate.Location = New System.Drawing.Point(4, 293)
+        Me.btnCreate.Name = "btnCreate"
+        Me.btnCreate.Size = New System.Drawing.Size(78, 30)
+        Me.btnCreate.TabIndex = 0
+        Me.btnCreate.Text = "Create"
+        Me.btnCreate.UseVisualStyleBackColor = True
+        '
+        'txtHeight
+        '
+        Me.txtHeight.Location = New System.Drawing.Point(308, 12)
+        Me.txtHeight.Name = "txtHeight"
+        Me.txtHeight.Size = New System.Drawing.Size(100, 27)
+        Me.txtHeight.TabIndex = 1
+        '
+        'txtCapacity
+        '
+        Me.txtCapacity.Location = New System.Drawing.Point(308, 54)
+        Me.txtCapacity.Name = "txtCapacity"
+        Me.txtCapacity.Size = New System.Drawing.Size(100, 27)
+        Me.txtCapacity.TabIndex = 2
+        '
+        'txtHourGlass
+        '
+        Me.txtHourGlass.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtHourGlass.Location = New System.Drawing.Point(104, 170)
+        Me.txtHourGlass.Multiline = True
+        Me.txtHourGlass.Name = "txtHourGlass"
+        Me.txtHourGlass.ReadOnly = True
+        Me.txtHourGlass.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.txtHourGlass.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.txtHourGlass.Size = New System.Drawing.Size(647, 240)
+        Me.txtHourGlass.TabIndex = 3
+        Me.txtHourGlass.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'lblHeight
+        '
+        Me.lblHeight.AutoSize = True
+        Me.lblHeight.Location = New System.Drawing.Point(80, 15)
+        Me.lblHeight.Name = "lblHeight"
+        Me.lblHeight.Size = New System.Drawing.Size(211, 20)
+        Me.lblHeight.TabIndex = 4
+        Me.lblHeight.Text = "Enter an integer greater than 1"
+        '
+        'lblCapacity
+        '
+        Me.lblCapacity.AutoSize = True
+        Me.lblCapacity.Location = New System.Drawing.Point(64, 57)
+        Me.lblCapacity.Name = "lblCapacity"
+        Me.lblCapacity.Size = New System.Drawing.Size(227, 20)
+        Me.lblCapacity.TabIndex = 5
+        Me.lblCapacity.Text = "Enter an integer between (0-100)"
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(4, 338)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 30)
+        Me.btnExit.TabIndex = 6
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblError1
+        '
+        Me.lblError1.AutoSize = True
+        Me.lblError1.ForeColor = System.Drawing.Color.Red
+        Me.lblError1.Location = New System.Drawing.Point(100, 102)
+        Me.lblError1.Name = "lblError1"
+        Me.lblError1.Size = New System.Drawing.Size(0, 20)
+        Me.lblError1.TabIndex = 7
+        '
+        'lblError2
+        '
+        Me.lblError2.AutoSize = True
+        Me.lblError2.ForeColor = System.Drawing.Color.Red
+        Me.lblError2.Location = New System.Drawing.Point(100, 147)
+        Me.lblError2.Name = "lblError2"
+        Me.lblError2.Size = New System.Drawing.Size(0, 20)
+        Me.lblError2.TabIndex = 8
+        '
+        'frmHourGlass
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(774, 422)
+        Me.Controls.Add(Me.lblError2)
+        Me.Controls.Add(Me.lblError1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.lblCapacity)
+        Me.Controls.Add(Me.lblHeight)
+        Me.Controls.Add(Me.txtHourGlass)
+        Me.Controls.Add(Me.txtCapacity)
+        Me.Controls.Add(Me.txtHeight)
+        Me.Controls.Add(Me.btnCreate)
+        Me.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "frmHourGlass"
+        Me.Text = "Hour Glass"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnCreate As System.Windows.Forms.Button
+    Friend WithEvents txtHeight As System.Windows.Forms.TextBox
+    Friend WithEvents txtCapacity As System.Windows.Forms.TextBox
+    Friend WithEvents txtHourGlass As System.Windows.Forms.TextBox
+    Friend WithEvents lblHeight As System.Windows.Forms.Label
+    Friend WithEvents lblCapacity As System.Windows.Forms.Label
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents lblError1 As System.Windows.Forms.Label
+    Friend WithEvents lblError2 As System.Windows.Forms.Label
+
+End Class
---
+++ 85a7af8ca55e0c752083358013f1edcd/hourglass/hourglass/Form1.vb
@@ -0,0 +1,222 @@
+﻿Public Class frmHourGlass
+
+    'declare constants
+    Const TOP_SHELF As Char = "_"
+    Const SAND As Char = "x"
+    Const LEFT_TOP As Char = "\"
+    Const RIGHT_TOP As Char = "/"
+    Const TEMP_BLANK_SPACE As Char = "_"
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        ' close the program
+        Me.Close()
+
+    End Sub
+
+    Private Sub btnCreate_Click(sender As Object, e As EventArgs) Handles btnCreate.Click
+
+        ' declare variables
+        Dim dblHeight As Double ' for height of hourglass
+        Dim dblCapacity As Double ' for sand in hourglass
+        Dim dblTopRowSpaces As Double ' to hold the amount of spaces in the top row
+        Dim dblTopHourglassSpaces As Double ' to hold the amount of spaces for the top of hourglass
+        Dim dblTotalSpaces As Double ' to hold the amount of total spaces
+        Dim strTopOutput As String ' to hold the top output of the hourglass
+        Dim strBottomOutput As String ' to hold the bottom output of the hourglass
+        Dim strTopShelf As String ' to hold the top of the hour glass
+        Dim dblSandSpaces As String ' to hold the number of spaces with sand
+        Dim dblBlankSpaces As String ' to hold the number of spaces with no sand
+
+
+
+        ' text to make sure height box has value and that it is over 1
+        If txtHeight.Text = String.Empty Then
+            'display error message if text box is empty
+            lblError1.Text = "Please enter an appropriate value"
+        ElseIf txtHeight.Text < 2 Then
+            ' display error message if criteria isn't met
+            lblError1.Text = "Please enter an appropriate value"
+            'test to be sure capacity box has value
+        ElseIf txtCapacity.Text = String.Empty Then
+            '  if empty display error message
+            lblError2.Text = "Please enter an appropriate value"
+            ' text to make sure capacity value is between 0 and 100
+        ElseIf txtCapacity.Text < 0 Or txtCapacity.Text > 100 Then
+            ' if not display error message
+            lblError2.Text = "Please enter an appropriate value"
+        Else
+            'get height
+            dblHeight = getHeight(txtHeight.Text)
+            ' get capacity
+            dblCapacity = getCapacity(txtCapacity.Text)
+            ' calculate spaces in top row
+            dblTopRowSpaces = getTopRow(dblHeight)
+            ' calculate the spaces on top of hourglass
+            dblTopHourglassSpaces = getTopHourglassSpaces(dblHeight)
+            'get the top shelf of hourglass
+            strTopShelf = drawTopHourGlass(dblTopHourglassSpaces)
+            ' calculate the total amount of spaces
+            dblTotalSpaces = calcTotalSpaces(dblTopRowSpaces)
+            ' calculate the number of sand spaces
+            dblSandSpaces = calcSandSpaces(dblTotalSpaces, dblCapacity)
+            ' calculate the number of blank spaces
+            dblBlankSpaces = calcEmptySpaces(dblSandSpaces, dblTotalSpaces)
+            strTopOutput = drawTopOutput(dblHeight, dblTopRowSpaces, dblTotalSpaces, dblSandSpaces)
+            'replace temporary blank space with blank space
+            strTopOutput = Replace(strTopOutput, TEMP_BLANK_SPACE, "  ")
+            strTopOutput = strTopShelf + strTopOutput
+            strBottomOutput = drawBottomOutput(dblHeight, dblTopRowSpaces, dblTotalSpaces, dblSandSpaces)
+            txtHourGlass.Text = strTopOutput + strBottomOutput
+
+        End If
+
+
+
+    End Sub
+
+    Private Sub txtHeight_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtHeight.KeyPress, txtCapacity.KeyPress
+        ' only allow numbers or backspace to be pressed in text boxes
+
+        If (e.KeyChar < "0" OrElse e.KeyChar > "9") AndAlso
+            e.KeyChar <> ControlChars.Back Then
+            e.Handled = True
+        End If
+
+    End Sub
+
+
+
+    Private Sub txtHeight_TextChanged(sender As Object, e As EventArgs) Handles txtHeight.TextChanged, txtCapacity.TextChanged
+        ' clear the error message if one appears
+        lblError1.Text = " "
+        lblError2.Text = " "
+        ' clear the hourglass
+        txtHourGlass.Text = " "
+    End Sub
+
+
+    Private Function getHeight(userHeight) As Double
+        Dim dblHeight As Double
+        ' parse height value to double variable
+        Integer.TryParse(userHeight, dblHeight)
+        ' return height value
+        Return dblHeight
+    End Function
+
+    Private Function getCapacity(userCapacity) As Double
+        Dim dblCapacity As Double
+        ' parse capacity value to double variable
+        Integer.TryParse(userCapacity, dblCapacity)
+        ' return capacity
+        Return dblCapacity
+    End Function
+
+    Private Function getTopRow(ByVal heightValue As Double) As Double
+        Dim dblAmtTopRow As Double
+        'calculate the value of the spaces in the top row and assign variable
+        dblAmtTopRow = (2 * heightValue) - 1
+        'return variable
+        Return dblAmtTopRow
+    End Function
+
+    Private Function getTopHourglassSpaces(ByVal heightValue As Double) As Double
+        Dim dblAmtTopGlass As Double
+        ' calculate the spaces on top of the hourglass and assingn value
+        dblAmtTopGlass = (2 * heightValue) + 1
+        'return variable
+        Return dblAmtTopGlass
+    End Function
+
+    Private Function drawTopHourGlass(ByVal shelfSpaces As Double) As String
+        Dim strTopShelf As String
+        ' run a loop to fill the line with the top shelf
+        For count As Integer = 1 To shelfSpaces
+            strTopShelf += TOP_SHELF
+        Next
+        strTopShelf += vbNewLine
+        Return strTopShelf
+    End Function
+
+    Private Function calcTotalSpaces(ByVal dblTopRowSpaces As Double) As Double
+        Dim dblTotalSpaces As String
+        ' run a decrementing loop to count the number of spaces
+        For count As Integer = dblTopRowSpaces To 1 Step -2
+            ' accumulate space total
+            dblTotalSpaces += count
+        Next
+        ' return amount of spaces
+        Return dblTotalSpaces
+    End Function
+
+    Private Function calcSandSpaces(ByVal dblTotal As Double, ByVal userCapacity As Double) As Double
+        ' variables
+        Dim dblSandSpaces As Double     ' to hold sand spaces
+        Dim dblPercent As Double    ' to hold percentage total
+        ' calculate the percentage to multiply the total amount by
+        dblPercent = (userCapacity * 0.01)
+        ' multiply the percentage by the total and round up
+        dblSandSpaces = Math.Ceiling(dblTotal * dblPercent)
+        ' return variable
+        Return dblSandSpaces
+    End Function
+
+    Private Function calcEmptySpaces(ByVal dblSubSand As Double, ByVal dblTotalSpace As Double) As Double
+        Dim dblEmptySpaces  ' to hold the number of empty spaces
+        ' calculate the amount of empty spaces
+        dblEmptySpaces = dblTotalSpace - dblSubSand
+        ' return variable
+        Return dblEmptySpaces
+    End Function
+
+    Private Function drawTopOutput(ByVal dblHourHeight As Double, ByVal dblTopSpaces As Double,
+                                   ByVal dblTotalSpaces As Double, ByVal dblSandSpaces As Double) As String
+        Dim strTopOutput As String = " "
+        Dim dblRowSpaces As Double = dblTopSpaces
+        For count = 1 To dblHourHeight
+            strTopOutput += LEFT_TOP
+            For index = 1 To dblTopSpaces
+                If (dblTotalSpaces / dblSandSpaces) > 1 Then
+                    strTopOutput += TEMP_BLANK_SPACE
+                    dblTotalSpaces -= 1
+                   
+                ElseIf (dblTotalSpaces / dblSandSpaces) <= 1 Then
+                    strTopOutput += SAND
+                    dblTotalSpaces -= 1
+                   
+                End If
+            Next
+
+            strTopOutput += RIGHT_TOP & ControlChars.NewLine
+            dblHourHeight -= 1
+            dblTopSpaces -= 2
+        Next
+        Return strTopOutput
+    End Function
+
+    Private Function drawBottomOutput(ByVal dblHourHeight As Double, ByVal dblTopSpaces As Double,
+                                   ByVal dblTotalSpaces As Double, ByVal dblSandSpaces As Double) As String
+        Dim strBottomOutput As String = " "
+        Dim dblSpace As Double = 1
+        dblTotalSpaces = 1
+        For count = 1 To dblHourHeight
+            strBottomOutput += RIGHT_TOP
+            For index = 1 To dblSpace
+                If (dblTotalSpaces / dblSandSpaces) > 1 Then
+                    strBottomOutput += SAND
+                    dblTotalSpaces += 1
+                ElseIf (dblTotalSpaces / dblSandSpaces) <= 1 Then
+                    strBottomOutput += "  "
+                    dblTotalSpaces += 1
+                End If
+            Next
+            strBottomOutput += LEFT_TOP & vbNewLine
+            dblHourHeight += 1
+            dblSpace += 2
+
+        Next
+        Return strBottomOutput
+    End Function
+
+End Class
+
+
